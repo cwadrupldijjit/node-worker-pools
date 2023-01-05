1c364d88583d5db5d7f36954087bd6cda919f7d8
Samuel Skeen <samuel.skeen@dynata.com>
2023-01-05 16:52:11 -0700

    simple sample of node worker pools

---
+++ index.ts
@@ -0,0 +1,51 @@
+import { isMainThread, Worker } from 'worker_threads';
+
+if (!isMainThread) {
+    console.error('This must be executed in the main thread');
+    process.exit(1);
+}
+
+const poolSize = 3;
+
+const pool = Array(poolSize).fill('')
+    .reduce((p, _, i) => {
+        const worker: Worker = new Worker('./worker/worker-entry.mjs', {
+            env: {
+                WORKER_ID: String(i),
+                NODE_NO_WARNINGS: '1',
+            },
+            execArgv: [
+                '--loader',
+                'ts-node/esm',
+                '-r',
+                'ts-node/register',
+            ],
+        });
+        worker.addListener('message', (message: string) => {
+            console.log('message from worker:', message);
+        });
+        return {
+            ...p,
+            ['worker' + i]: worker,
+        };
+    }, {});
+
+let currentWorker = 0;
+
+process.stdin.on('data', (data) => {
+    const input = data.toString('utf-8').trim();
+    
+    if (input == 'exit') {
+        process.exit(0);
+    }
+    
+    const worker: Worker = pool['worker' + currentWorker];
+    
+    advanceWorker();
+    
+    worker.postMessage(input);
+});
+
+function advanceWorker() {
+    currentWorker = (currentWorker + 1) % poolSize;
+}
---
+++ worker/worker.ts
@@ -0,0 +1,12 @@
+import { isMainThread, parentPort } from 'worker_threads';
+
+if (isMainThread) {
+    console.error('This should never be run directly');
+    process.exit(1);
+}
+
+parentPort.on('message', (message: string) => {
+    console.log('Hello from worker %s.  Your input was %s.', process.env.WORKER_ID, message);
+    
+    parentPort.postMessage(Buffer.from(message).toString('hex'));
+});
